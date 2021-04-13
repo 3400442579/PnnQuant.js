f3297de291be779d54bffa7db35dbcb556034b4a
Miller Cy Chan <miller.chan@gmail.com>
2021-04-13 17:16:44 +0800

    Add files via upload

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -208,7 +208,7 @@ function drawImageScaled(img){
 	ctx.filter = "blur" + steps + "px";
 	ctx.drawImage(img, 0, 0, width, height,
         0, 0, can.width,  can.height);
-	img.width = can.width; img.height = can.height;
+	$(img).css({width: "100%", height: "100%"});
     return can.toDataURL();
 }
 
