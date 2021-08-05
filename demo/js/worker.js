e23021c1bc494b97cb02cb881c9995390f3d8dda
Miller Cy Chan <miller.chan@gmail.com>
2021-08-05 16:20:30 +0800

    Add files via upload

--- demo/js/worker.js
+++ demo/js/worker.js
@@ -5,13 +5,14 @@ importScripts('pnnLABquant.min.js');
 
 function quantizeImage(opts) {				
 	var quant = opts.isHQ ? new PnnLABQuant(opts) : new PnnQuant(opts);
-	if(opts.isHQ) {
-		if(opts.dithering)
-			return { img8: quant.quantizeImage(), pal8: quant.getPalette(), indexedPixels: quant.getIndexedPixels(), transparent: quant.getTransparentIndex(), type: quant.getImgType() };
-		
+	if(opts.dithering)
+		return { img8: quant.quantizeImage(), pal8: quant.getPalette(), indexedPixels: quant.getIndexedPixels(), transparent: quant.getTransparentIndex(), type: quant.getImgType() };
+	
+	var pal8;
+	if(opts.isHQ) {		
 		opts.ditherFn = quant.getDitherFn();
 		opts.getColorIndex = quant.getColorIndex;		
-		var pal8;
+		
 		if(opts.colors < 64) {
 			quant.quantizeImage();
 			pal8 = quant.getPalette();
@@ -22,11 +23,13 @@ function quantizeImage(opts) {
 			opts.paletteOnly = true;
 			opts.palette = pal8 = quant.quantizeImage();	
 			opts.indexedPixels = new HilbertCurve(opts).dither();
-		}
-		var bn = new BlueNoise(opts);
-		return { img8: bn.dither(), pal8: pal8, indexedPixels: bn.getIndexedPixels(), transparent: quant.getTransparentIndex(), type: quant.getImgType() };
+		}		
 	}
-	return { img8: quant.quantizeImage(), pal8: quant.getPalette(), indexedPixels: quant.getIndexedPixels(), transparent: quant.getTransparentIndex(), type: quant.getImgType() };
+	else
+		pal8 = quant.getPalette();
+	
+	var bn = new BlueNoise(opts);
+	return { img8: bn.dither(), pal8: pal8, indexedPixels: bn.getIndexedPixels(), transparent: quant.getTransparentIndex(), type: quant.getImgType() };
 }
 
 onmessage = function(e) {	
