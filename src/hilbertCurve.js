af7f6000e137ef037c444065b52d21a2e9f893bc
Miller Cy Chan <miller.chan@gmail.com>
2021-08-16 10:47:26 +0800

    Add files via upload

--- src/hilbertCurve.js
+++ src/hilbertCurve.js
@@ -77,10 +77,11 @@ Copyright (c) 2021 Miller Cy Chan
 	        	if(Math.abs(error.p[j]) < DITHER_MAX)
 					continue;
 				
-				if(nMaxColors < 64)
-	        		error.p[j] = error.p[j] < 0 ? -DITHER_MAX + 1 : DITHER_MAX - 1;
-				else
-					error.p[j] -= error.p[j] < 0 ? -DITHER_MAX : DITHER_MAX;
+				error.p[j] -= error.p[j] < 0 ? -DITHER_MAX : DITHER_MAX;
+				if(Math.abs(error.p[j]) < DITHER_MAX)
+					continue;
+				
+	        	error.p[j] = error.p[j] < 0 ? -DITHER_MAX + 1 : DITHER_MAX - 1;				
 	        }
 	        errorq.push(error);
 	    }
