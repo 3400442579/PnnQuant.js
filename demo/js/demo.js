3a229d0a3c1c7a98508f2b4f18c432bceb0d94c3
Miller Cy Chan <miller.chan@gmail.com>
2021-11-04 10:53:02 +0800

    Add files via upload

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -104,22 +104,10 @@ function allowChange($orig) {
 
 function getResult(opts) {
 	var quant = opts.isHQ ? new PnnLABQuant(opts) : new PnnQuant(opts);	
-	
 	opts.ditherFn = quant.getDitherFn();
 	opts.getColorIndex = quant.getColorIndex;
-	
-	if (opts.colors < 64 && opts.colors > 32) {
-		if(opts.dithering)
-			return Promise.all([quant.getResult()]);
-		
-		return quant.getResult().then(function(result) {
-			opts.palette = new Uint32Array(result.pal8);
-			opts.indexedPixels = result.indexedPixels;				
-			return Promise.all([result, new BlueNoise(opts).getResult()]);
-		});				
-	}
-
 	opts.paletteOnly = true;
+	
 	return quant.getResult().then(function(result) {		
 		opts.palette = result.pal8;
 		
--- demo/js/worker.js
+++ demo/js/worker.js
@@ -5,22 +5,10 @@ importScripts('pnnLABquant.min.js');
 
 function quantizeImage(opts) {				
 	var quant = opts.isHQ ? new PnnLABQuant(opts) : new PnnQuant(opts);	
-	
 	opts.ditherFn = quant.getDitherFn();
 	opts.getColorIndex = quant.getColorIndex;
-	
-	if (opts.colors < 64 && opts.colors > 32) {
-		if(opts.dithering)
-			return Promise.all([quant.getResult()]);
-		
-		return quant.getResult().then(function(result) {
-			opts.palette = new Uint32Array(result.pal8);
-			opts.indexedPixels = result.indexedPixels;				
-			return Promise.all([result, new BlueNoise(opts).getResult()]);
-		});				
-	}
-
 	opts.paletteOnly = true;
+	
 	return quant.getResult().then(function(result) {
 		opts.palette = result.pal8;
 		
